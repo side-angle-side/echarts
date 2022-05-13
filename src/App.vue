4e94c5cf8846de7c26f525c658577220c8e850a7
side-angle-side <1900011019@pku.edu.cn>
2022-05-13 22:15:18 +0800

    add echarts

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <v-app>
-    <!-- <v-app-bar color="grey-lighten-2"></v-app-bar>
-    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer> -->
     <v-main>
-      <!-- <v-card height="200px"></v-card> -->
-      <helloWorld />
+      <BarChart />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import BarChart from "./components/BarChart.vue";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    BarChart,
   },
 
   data: () => ({
---
+++ src/components/BarChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <div id="main" style="height: 500px"></div>
+  </v-container>
+</template>
+
+<script>
+import * as echarts from "echarts";
+import { onMounted } from "vue";
+
+export default {
+  name: "BarChart",
+  data: () => ({}),
+  setup: () => {
+    onMounted(() => {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById("main"));
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      });
+    });
+  },
+};
+</script>
